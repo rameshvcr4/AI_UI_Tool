306fc337460f556907f1fb76276f5c41072d02d0
Ramesh V <rameshvcr4@gmail.com>
2025-10-07 12:11:54 +0530

    Recreate and upload full project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/AI_UI_Tool/'   // IMPORTANT for GitHub Pages when repo name is AI_UI_Tool
 })
